ebd67b7b8687cd5acb6ba4fe3ba7bbf755e3a499
Ashkan <ashkanehsani04@gmail.com>
2026-01-04 16:33:52 +0330

    Add player struct

--- phase1.c
+++ phase1.c
@@ -2,6 +2,17 @@
 #include <math.h>
 #include "raylib.h"
 
+typedef struct{
+    float x;
+    float y;
+}Vector;
+
+typedef struct{
+    Vector pos;
+    Vector dir;
+    Vector plane;
+}Player;
+
 int main(){
     return 0;
 }
