0fc1ac026e970fdfa160c1fd21ef2864311e37c5
Ashkan <ashkanehsani04@gmail.com>
2026-01-04 16:39:07 +0330

    Add player primary information

--- phase1.c
+++ phase1.c
@@ -17,5 +17,13 @@ int main(){
     InitWindow(1200, 900, "game window");
     Color cb1 = {0, 50, 250, 80};
     Color cr1 = {180, 0, 0, 255};
+    Player player;
+    player.pos.x = 600.0f;
+    player.pos.y = 450.0f;
+    player.dir.x = 1.0f;
+    player.dir.y = 0.0f;
+    player.plane.x = 0.0f;
+    player.plane.y = 0.66f;
+    SetTargetFPS(60);
     return 0;
 }
